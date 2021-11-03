0fc812998dc652093e85cad0f822e20c8d7e041f
freddieswift <freddie9117@live.co.uk>
2021-11-03 16:00:09 +0000

    Added delete end points

---
+++ src/routers/task.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const Task = require('../models/task')
+
+const router = new express.Router()
+
+//Create Task
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send()
+    }
+    catch (error) {
+        res.status(400).send(e)
+    }
+})
+
+//Get all tasks
+router.get('/tasks', async (req, res) => {
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//Get specific task
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send
+        }
+        res.send(task)
+    }
+    catch (error) {
+        res.status(500).send()
+    }
+})
+
+//update task
+router.patch('/tasks/:id', async (req, res) => {
+    //check if the update is valid by comparing the field to be updated with the fields present on the Task model
+    //if the field to be updated is not on the Task model, then return an error
+    const updates = Object.keys(req.body)
+    let allowedUpdates = Object.keys(Task.schema.paths)
+    allowedUpdates = allowedUpdates.filter(item => item != '_id')
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates'})
+    }
+
+    //update the task, specified by the id provided, with data provided in the request body
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//Delete Tasks
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch (error) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
---
+++ src/routers/user.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const User = require('../models/user')
+
+const router = new express.Router()
+
+//Create User
+//Get user data from request body
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+//Get all users
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch (error) {
+        res.status(500).send(users)
+    }
+})
+
+//Get specific user
+//Get id of user to find from paremter in URL
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(400).send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
+})
+
+//update user
+router.patch('/users/:id', async (req, res) => {
+    //checks if the update is valid by comparing the field to be updated with the fields present on the User model
+    //if the field to be updated is not on the User model, then return an error
+    const updates = Object.keys(req.body)
+    let allowedUpdates = Object.keys(User.schema.paths)
+    allowedUpdates = allowedUpdates.filter(item => item != '_id')
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    ////update the user, specified by the id provided, with data provided in the request body
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            return res.send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+
+//Delete User
+router.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try{
+        const user = await User.findByIdAndDelete(_id)
+        if (!user){
+            res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
